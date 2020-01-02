2e24149121db9dea7bb52c6c10573289ef2aac93
Anders Busch <andersbusch@gmail.com>
2020-01-02 22:56:57 +0100

    added acceptor with asio

--- src/main.cpp
+++ src/main.cpp
@@ -23,24 +23,27 @@ static void penv(const char * const * envp, std::stringstream &stream)
     stream << "</PRE>\n";
 }
 
-int main (void)
-{
-    int count = 0;
-    long pid = getpid();
+class Acceptor {
+    asio::io_context &m_io;
+    int m_count = 0;
+    long m_pid = 0;
 
-    FCGX_Init();
+public:
+    Acceptor(asio::io_context &io) : m_io(io) {
+        m_pid = getpid();
+    }
 
-    while (true) {
+    void operator() () {
         auto sout = std::stringstream();
         auto request = FcgiRequest::create();
 
-        if ( !request->accept() ) break;
+        if ( !request->accept() ) return;
 
         sout << "Content-type: text/html\r\n\r\n"
                 "<TITLE>echo-cpp</TITLE>\n"
                 "<H1>echo-cpp</H1>\n"
-                "<H4>PID: " << pid << "</H4>\n"
-                "<H4>Request Number: " << ++count << "</H4>\n";
+                "<H4>PID: " << m_pid << "</H4>\n"
+                "<H4>Request Number: " << ++m_count << "</H4>\n";
 
         sout << "<H4>Process/Initial Environment</H4>\n";
         penv(environ, sout);
@@ -51,7 +54,22 @@ int main (void)
         auto val = sout.str();
 
         FCGX_PutStr(val.c_str(), val.size(), request->out());
+
+        m_io.post(*this);
     }
+};
+
+int main(void) {
+
+    asio::io_context io;
+
+    auto acceptor = Acceptor(io);
+
+    FCGX_Init();
+
+    io.post(acceptor);
+
+    io.run();
 
     return 0;
 }
