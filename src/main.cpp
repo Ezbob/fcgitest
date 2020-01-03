c74a644fdb0e8d4e726787982cb96d839932a3b1
Anders Busch <andersbusch@gmail.com>
2020-01-03 11:52:26 +0100

    handler is now constructored in acceptor

--- src/main.cpp
+++ src/main.cpp
@@ -22,39 +22,36 @@ static void penv(const char * const * envp, std::stringstream &stream)
     stream << "</PRE>\n";
 }
 
+static long g_pid = getpid();
+static int g_count = 0;
+
 class Handler {
-    int m_count = 0;
-    long m_pid = 0;
-
+    std::shared_ptr<FcgiRequest> m_request;
 public:
-    Handler() {
-        m_pid = getpid();
-    }
+    Handler(std::shared_ptr<FcgiRequest> r) : m_request(r) {}
 
-    void operator() (std::shared_ptr<FcgiRequest> request) {
-        auto sout = std::stringstream();
-
-        sout << "Content-type: text/html\r\n\r\n"
+    void operator() () {
+        std::stringstream out;
+        out << "Content-type: text/html\r\n\r\n"
                 "<TITLE>echo-cpp</TITLE>\n"
                 "<H1>echo-cpp</H1>\n"
-                "<H4>PID: " << m_pid << "</H4>\n"
-                "<H4>Request Number: " << ++m_count << "</H4>\n";
+                "<H4>PID: " << g_pid << "</H4>\n"
+                "<H4>Request Number: " << ++g_count << "</H4>\n";
 
-        sout << "<H4>Process/Initial Environment</H4>\n";
-        penv(environ, sout);
+        out << "<H4>Process/Initial Environment</H4>\n";
+        penv(environ, out);
 
-        sout << "<H4>Fcgi Environment</H4>\n";
-        penv(request->envp(), sout);
+        out << "<H4>Fcgi Environment</H4>\n";
+        penv(m_request->envp(), out);
 
-        auto val = sout.str();
+        auto val = out.str();
 
-        FCGX_PutStr(val.c_str(), val.size(), request->out());
+        FCGX_PutStr(val.c_str(), val.size(), m_request->out());
     }
 };
 
 class Acceptor {
     asio::io_context &m_io;
-    Handler m_handler;
 
 public:
     Acceptor(asio::io_context &io) : m_io(io) {}
@@ -64,9 +61,7 @@ public:
 
         if ( !request->accept() ) return;
 
-        m_io.post([this, request]() {
-            m_handler(request);
-        });
+        m_io.post(Handler(request));
 
         m_io.post(*this);
     }
