71e9b6a2f6a2135e3e911eed170a7df92c2db581
Anders Busch <andersbusch@gmail.com>
2020-01-15 20:48:35 +0100

    more basic stuff

--- include/fcgipp/authenticator.hpp
+++ include/fcgipp/authenticator.hpp
@@ -3,15 +3,11 @@
 #ifndef _HEADER_FILE_authenticator_20200103164302_
 #define _HEADER_FILE_authenticator_20200103164302_
 
+#include "basic_authenticator.hpp"
 #include "fcgi_req_res.hpp"
 #include <memory>
 
 namespace fcgipp {
-
-    struct BasicAuthenticator {
-        virtual ~BasicAuthenticator() = default;
-        virtual bool is_valid(std::shared_ptr<FcgiReqRes> const&) = 0;
-    };
 
     class DefaultAuthenticator : public BasicAuthenticator {
     public:
---
+++ include/fcgipp/basic_authenticator.hpp
@@ -0,0 +1,17 @@
+
+#ifndef _HEADER_FILE_basic_authenticator_20200115204611_
+#define _HEADER_FILE_basic_authenticator_20200115204611_
+
+#include <memory>
+#include "fcgi_req_res.hpp"
+
+namespace fcgipp {
+
+    struct BasicAuthenticator {
+        virtual ~BasicAuthenticator() = default;
+        virtual bool is_valid(std::shared_ptr<FcgiReqRes> const&) = 0;
+    };
+
+};
+
+#endif
