78d520ef5e217c29448dc8b136719e31bf169530
Anders Busch <andersbusch@gmail.com>
2020-01-21 20:42:41 +0100

    removed fcgisrv source and added library

--- src/fcgisrv/default_authenticator.cpp
+++
@@ -1,9 +0,0 @@
-
-
-#include "fcgisrv/default_authenticator.hpp"
-
-using namespace fcgisrv;
-
-bool DefaultAuthenticator::is_valid(std::shared_ptr<BasicServerRequestResponse> const &) {
-    return true;
-}
